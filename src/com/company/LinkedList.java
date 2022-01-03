68a8d541dad5521597487b18eba0ec4b71625ed2
Edgar Moran <edgarm@twitter.com>
2022-01-03 09:52:23 -0500

    removing duplicates from Node

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -1,17 +1,52 @@
 package com.company;
 
+import java.util.*;
+import java.lang.*;
+
 public class LinkedList {
+
+  static class LinkedListNode<E> {
+    E data;
+    LinkedListNode<E> next;
+  }
 
   public static void main(String[] args){
     Node node = new Node(5);
 
     node.appendToTail(3);
+    node.appendToTail(3);
+    node.appendToTail(8);
     node.appendToTail(6);
+    node.appendToTail(5);
+    node.appendToTail(5);
 
+
+
+
+
+    removeDuplicates(node);
 
     System.out.println("Nodes"+node);
-
   }
+
+  public static Node removeDuplicates(Node n){
+    HashSet<Integer> set = new HashSet<Integer>();
+    Node previous = null;
+
+    while (n != null){
+      if(set.contains(n.data)){
+        previous.next = n.next;
+      }
+      else{
+        set.add((Integer) n.data);
+        previous = n;
+      }
+      n = n.next;
+    }
+
+    return n;
+  }
+
 
   public static class Node{
     int data;
@@ -21,11 +56,11 @@ public class LinkedList {
       this.data = data;
     }
 
-    public void appendToTail(int d){
+    public void appendToTail(int d) {
       Node end = new Node(d);
       Node n = this;
 
-      while(n.next != null){
+      while (n.next != null) {
         n = n.next;
       }
       n.next = end;
